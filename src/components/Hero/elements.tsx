b37439763115490703800bfa661056d84cae9a01
hendricku <hendrickpaul.castro@lorma.edu>
2025-09-18 14:23:51 +0800

    added new theme

--- src/components/Hero/elements.tsx
+++ src/components/Hero/elements.tsx
@@ -2,10 +2,11 @@
 
 import { styled } from "@mui/material/styles";
 
-export const HeroSection = styled("section")(() => ({
+export const HeroSection = styled("section")(({ theme }) => ({
   width: "100%",
   maxWidth: 1440,
   margin: "0 auto",
+  background: theme.palette.navy.main,
   // backgroundColor: theme.palette.common.black,
   position: "relative",
 
